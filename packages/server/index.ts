26698d0a2797193e7dbe0f22638c83db819d7f0b
Xavier <Xaviermaldonadony@hotmail.com>
2025-10-20 22:26:55 -0400

    managed env

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,12 +1,15 @@
 import express, { response } from 'express';
 import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT || 300;
 
 app.get('/', (req: Request, resp: Response) => {
-  resp.send('hello world');
+  resp.send(process.env.OPENAI_API_KEY);
 });
 
 app.listen(port, () => {
